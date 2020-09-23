611d975103eac742787887ee732650ebd6eb16f5
BuildTools <unconfigured@null.spigotmc.org>
2020-09-23 10:38:39 +0200

    adding CustomWelcomeMessage

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
     @Override
     public void onDisable() {
-        super.onDisable();
+
     }
 
     @Override
     public void onEnable() {
-        super.onEnable();
+
     }
 }
